5cf41423112cd2aa1256567b10ad9888cfd791d2
frsiam <siam.frs@gmail.com>
2022-04-04 20:16:26 +0600

    create custom hook and load the customer data from fakedata.json and use it into Home  component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import laptop from '../../images/laptop.jpg'
+import useCustomer from '../hooks/useCustomer';
 
 const Home = () => {
+    const [customers] = useCustomer()
+    console.log(customers[0])
     return (
         <div className='p-3 border-4 mx-20 grid grid-cols-2'>
             <div className='border-2 grid justify-start pl-8 h-auto content-center py-5'>
-                <h1>left side</h1>
+                <h1>left side: {customers.length}</h1>
+                <img src={customers[0].img} alt="" />
                 <h1 className='text-4xl font-bold'><span className='text-pink-500'>Your Product</span> <br />Our Analysis</h1>
             </div>
             <div className='border-2 grid justify-end pr-8 h-auto content-center py-5'>
---
+++ src/components/hooks/useCustomer.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react"
+
+const useCustomer = () => {
+    const [customers,setCustomers] = useState([])
+
+    useEffect(() => {
+        fetch('fakedata.json')
+        .then(res => res.json())
+        .then(data => setCustomers(data))
+    },[])
+    return [customers, setCustomers];
+}
+
+export default useCustomer;
