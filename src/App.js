b9bbafaf76e6b1375bf09ef27848a5ab518a7ce4
frsiam <siam.frs@gmail.com>
2022-04-05 12:34:42 +0600

    add about component and show

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
 import Dashboard from './components/Dashboard/Dashboard';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Home/>}></Route>
         <Route path='/reviews' element={<Reviews></Reviews>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
         <Route path='*' element={<Notfound></Notfound>}></Route>
       </Routes>
     </>
---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='grid justify-center md:mx-20 md:my-32'>
+            <img src="https://downloadfreethemes.top/wp-content/uploads/2019/01/f0943110-underconstructionpage.png" alt="" />
+        </div>
+    );
+};
+
+export default About;
